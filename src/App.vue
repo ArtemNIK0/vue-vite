e9b018287e85be6e4bdd59cb472f78a6bb8043c6
ArtemNIK0 <knightroks27gmail.com>
2023-10-19 08:42:24 +0300

    img

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
   <div class="bg-red-700 font-sans m-0 h-screen">
     <div class="empty-div flex bg-gray-300 p-5 justify-center"></div>
     <div class="container mx-auto flex items-center justify-center bg-red-600 p-5 h-24 text-center">
-      <button @click="openModal" class="btr rounded-2xl bg-transparent mb-5 w-24 h-10 leading-10 text-black border-2 border-black font-bold">окно</button>
+      <button @click="openModal" class="btr rounded-2xl bg-transparent mb-5 w-24 h-10 px-2 py-1 leading-10 text-black border-2 border-black font-bold flex items-center justify-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag mr-2" viewBox="0 0 16 16">
+      <path fill="#444" d="M0 1v14h16v-14h-16zM15 14h-14v-10h14v10zM15 3h-1v-1h1v1z"></path>
+    </svg>
+    окно
+</button>
       <transition name="fade" appear>
         <div v-if="showModal" @click.self="closeModal" class="modal fixed top-0 left-0 w-full h-full bg-blue-400 bg-opacity-70 z-10 overflow-auto box-border">
           <div class="modal-content bg-black rounded-lg mx-auto p-10 w-80 max-w-md relative box-border">
