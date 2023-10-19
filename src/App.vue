461ed7b9b7bea6db19e1490a3402ef5645435d89
ArtemNIK0 <knightroks27gmail.com>
2023-10-19 11:33:00 +0300

    pravki

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <script setup>
+import { ref } from 'vue';
+
+const showModal = ref(false);
+
+const openModal = () => {
+  showModal.value = true;
+  document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  showModal.value = false;
+  document.body.style.overflow = "auto";
+};
 </script>
 
 <template>
@@ -6,11 +19,12 @@
     <div class="empty-div flex bg-gray-300 p-5 justify-center"></div>
     <div class="container mx-auto flex items-center justify-center bg-red-600 p-5 h-24 text-center">
       <button @click="openModal" class="btr rounded-2xl bg-transparent mb-5 w-24 h-10 px-2 py-1 leading-10 text-black border-2 border-black font-bold flex items-center justify-center">
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag mr-2" viewBox="0 0 16 16">
-      <path fill="#444" d="M0 1v14h16v-14h-16zM15 14h-14v-10h14v10zM15 3h-1v-1h1v1z"></path>
-    </svg>
-    окно
-</button>
+       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag mr-2" viewBox="0 0 16 16">
+         <path fill="#444" d="M0 1v14h16v-14h-16zM15 14h-14v-10h14v10zM15 3h-1v-1h1v1z"></path>
+       </svg>
+       окно
+      </button>
+      
       <transition name="fade" appear>
         <div v-if="showModal" @click.self="closeModal" class="modal fixed top-0 left-0 w-full h-full bg-blue-400 bg-opacity-70 z-10 overflow-auto box-border">
           <div class="modal-content bg-black rounded-lg mx-auto p-10 w-80 max-w-md relative box-border">
@@ -27,33 +41,7 @@
     </div>
   </div>
 </template>
-<script>
-import { ref } from 'vue';
 
-
-export default {
-  name: 'App',
-  setup() {
-    const showModal = ref(false);
-    
-    const openModal = () => {
-      showModal.value = true;
-      document.body.style.overflow = "hidden";
-    };
-
-    const closeModal = () => {
-      showModal.value = false;
-      document.body.style.overflow = "auto";
-    };
-
-    return {
-      showModal,
-      openModal,
-      closeModal,
-    };
-  },
-};
-</script>
 <style scoped>
 .fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
